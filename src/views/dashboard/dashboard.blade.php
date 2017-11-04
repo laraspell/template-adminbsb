db7157c48ad81b6544895926303898cd8a427731
Muhammad Syifa <emsifa@gmail.com>
2017-11-04 12:50:35 +0700

    Add card and infobox component. And enhance page dashboard

--- src/views/dashboard/dashboard.blade.php
+++ src/views/dashboard/dashboard.blade.php
@@ -1,13 +1,73 @@
 @extends('admin::layout.master')
 
 @section('content')
-@include('admin::partials.alert-messages')
-<div class="block-header">
-    <h2>Dashboard</h2>
-</div>
-<div class="card">
-  <div class="body">
-    <h4>This is default dashboard page.</h4>
+  @include('admin::partials.alert-messages')
+  
+  <div class="block-header">
+      <h2>Dashboard</h2>
+  </div>
+  
+  <div class="row">
+    <div class="col-md-3">
+      @include('admin::partials.infobox', [
+        'icon' => 'home',
+        'hover_effect' => 'zoom',
+        'count' => 50,
+        'label' => 'Count Data',
+        'icon_classes' => 'bg-blue',
+      ])
+    </div>
+    <div class="col-md-3">
+      @include('admin::partials.infobox', [
+        'icon' => 'home',
+        'hover_effect' => 'expand',
+        'count' => 50,
+        'label' => 'Count Data',
+        'color' => 'blue',
+      ])
+    </div>
+    <div class="col-md-3">
+      @include('admin::partials.infobox', [
+        'icon' => 'home',
+        'hover_effect' => 'expand',
+        'count' => 50,
+        'label' => 'Count Data',
+        'color' => 'blue',
+        'right_icon' => true,
+        'solid_color' => true,
+      ])
+    </div>
+    <div class="col-md-3">
+      @include('admin::partials.infobox', [
+        'icon' => 'home',
+        'hover_effect' => 'zoom',
+        'count' => 50,
+        'label' => 'Count Data',
+        'right_icon' => true,
+        'icon_color' => 'blue'
+      ])
+    </div>
+  </div>
+
+  @component('admin::partials.card', [
+    'title' => 'Dashboard',
+    'description' => 'This is default dashboard page.'
+  ])
+    @slot('header_dropdown')
+    <ul class="header-dropdown m-r--5">
+      <li class="dropdown">
+        <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true">
+          <i class="material-icons">more_vert</i>
+        </a>
+        <ul class="dropdown-menu pull-right">
+          <li><a href="javascript:void(0);" class=" waves-effect waves-block">Action</a></li>
+          <li><a href="javascript:void(0);" class=" waves-effect waves-block">Another action</a></li>
+          <li><a href="javascript:void(0);" class=" waves-effect waves-block">Something else here</a></li>
+        </ul>
+      </li>
+    </ul>
+    @endslot
+
     <table class="table table-striped table-bordered no-margin">
       <tr>
         <td width="120">Route Name</td>
@@ -25,6 +85,6 @@
         <td><code>{? schema.view.path ?}/dashboard/dashboard.blade.php</code></td>
       </tr>
     </table>
-  </div>
-</div>
+  @endcomponent
+
 @stop
