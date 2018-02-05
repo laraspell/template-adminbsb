562509bc30a6b4939913616740b15d326fb91775
Muhammad Syifa <emsifa@gmail.com>
2018-02-05 10:25:45 +0700

    Enhance sidebar menu

--- src/views/layout/sidebar-menu.blade.php
+++ src/views/layout/sidebar-menu.blade.php
@@ -2,12 +2,7 @@
   <ul class="list">
     <li class="header">MAIN NAVIGATION</li>
     @foreach(config('{? config_key ?}.menu') as $menu)
-    <li class="{{ Request::route()->getName() == $menu['route']? 'active' : '' }}">
-      <a href="{{ route($menu['route']) }}">
-        <i class="material-icons">{{ $menu['icon'] }}</i>
-        <span>{{ $menu['label'] }}</span>
-      </a>
-    </li>
+      @include('{? view_namespace ?}layout.sidebar-menu-item', ['menu' => $menu])
     @endforeach
   </ul>
 </div>
